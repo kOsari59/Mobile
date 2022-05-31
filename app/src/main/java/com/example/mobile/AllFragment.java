eecea89aa5d8982ddea57d3484b18e3f69bfaae6
kOsari5948 <86839047+kOsari5948@users.noreply.github.com>
2022-05-31 17:14:34 +0900

    camera

--- app/src/main/java/com/example/mobile/AllFragment.java
+++ app/src/main/java/com/example/mobile/AllFragment.java
@@ -1,5 +1,7 @@
 package com.example.mobile;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,13 +9,40 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class AllFragment extends Fragment {
+    View dialogView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all, container, false);
+
+        View fragment = inflater.inflate(R.layout.fragment_all, container, false);
+
+        Button btn = fragment.findViewById(R.id.button1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                dialogView = getLayoutInflater().inflate(R.layout.info,null);
+                AlertDialog mDialog = (AlertDialog) new Dialog(v.getContext());
+                mDialog.setTitle("상세 정보");
+                mDialog.setContentView(dialogView);
+                mDialog.setCancelable(true);
+                WindowManager.LayoutParams params = mDialog.getWindow().getAttributes();
+                params.width = WindowManager.LayoutParams.MATCH_PARENT;
+                params.height = WindowManager.LayoutParams.MATCH_PARENT;
+                mDialog.getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
+                mDialog.show();
+            }
+        });
+
+        return fragment;
     }
 }
